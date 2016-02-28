dbcc52e154c223223c47c11c54c9e434a9508877
Richy <richygreat@gmail.com>
2016-02-28 21:52:56 +0530

    Selenium stub simple

--- seleniumutil/src/main/java/test/Test.java
+++ seleniumutil/src/main/java/test/Test.java
@@ -1,9 +1,25 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
+	private WebDriver driver = new ChromeDriver();
 
 	public static void main(String[] args) {
-		throw new RuntimeException("Success in Exception");
+		Test test = new Test();
+		WebDriver driver = test.getDriver();
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
 	}
 
 }
