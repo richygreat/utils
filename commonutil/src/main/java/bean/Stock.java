8efa4e08eee41457cb3b509be57b70d2ec363199
Richy <richygreat@gmail.com>
2016-06-04 00:01:37 +0530

    Stock Analyser starts

---
+++ commonutil/src/main/java/bean/Stock.java
@@ -0,0 +1,66 @@
+package bean;
+
+public class Stock {
+	private String companyName;
+	private String industry;
+	private Double lastPrice;
+	private Double changeInPrice;
+	private Double changeInPercent;
+	private Double marketCapital;
+
+	@Override
+	public String toString() {
+		return "Stock [companyName=" + companyName + ", industry=" + industry + ", lastPrice=" + lastPrice
+				+ ", changeInPrice=" + changeInPrice + ", changeInPercent=" + changeInPercent + ", marketCapital="
+				+ marketCapital + "]";
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getIndustry() {
+		return industry;
+	}
+
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+
+	public Double getLastPrice() {
+		return lastPrice;
+	}
+
+	public void setLastPrice(Double lastPrice) {
+		this.lastPrice = lastPrice;
+	}
+
+	public Double getChangeInPrice() {
+		return changeInPrice;
+	}
+
+	public void setChangeInPrice(Double changeInPrice) {
+		this.changeInPrice = changeInPrice;
+	}
+
+	public Double getChangeInPercent() {
+		return changeInPercent;
+	}
+
+	public void setChangeInPercent(Double changeInPercent) {
+		this.changeInPercent = changeInPercent;
+	}
+
+	public Double getMarketCapital() {
+		return marketCapital;
+	}
+
+	public void setMarketCapital(Double marketCapital) {
+		this.marketCapital = marketCapital;
+	}
+
+}
---
+++ commonutil/src/main/java/parser/MoneyControlTableParser.java
@@ -0,0 +1,62 @@
+package parser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import bean.Stock;
+
+public class MoneyControlTableParser {
+	public static void main(String[] args) {
+		try {
+			List<Stock> lsStocks = new ArrayList<Stock>();
+			Stock stock = null;
+			Document doc = Jsoup
+					.connect(
+							"http://www.moneycontrol.com/stocks/marketstats/indexcomp.php?optex=BSE&opttopic=indexcomp&index=26")
+					.get();
+			for (Element elem : doc.select(".tbldata14")) {
+				for (Element tr : elem.getElementsByTag("tr")) {
+					int i = 0;
+					stock = new Stock();
+					for (Element td : tr.getElementsByTag("td")) {
+						switch (i) {
+						case 0:
+							stock.setCompanyName(td.select("a b").text());
+							break;
+						case 1:
+							stock.setIndustry(td.select("a b").text());
+							break;
+						case 2:
+							stock.setLastPrice(Double.valueOf(td.text().replace(",","")));
+							break;
+						case 3:
+							stock.setChangeInPrice(Double.valueOf(td.text().replace(",","")));
+							break;
+						case 4:
+							stock.setChangeInPercent(Double.valueOf(td.text().replace(",","")));
+							break;
+						case 5:
+							stock.setMarketCapital(Double.valueOf(td.text().replace(",","")));
+							break;
+						default:
+							break;
+						}
+						i++;
+					}
+					System.out.println(stock);
+					lsStocks.add(stock);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
